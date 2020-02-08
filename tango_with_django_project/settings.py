7db80c5b7816c4bbbe87a5e23eca46ef47d2ad34
Stephanie Matheson <stephanie.matheson79@gmail.com>
2020-02-08 14:33:37 +0000

    Chapter 4 complete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,10 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = [STATIC_DIR, ]
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 """tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,12 +20,9 @@ from django.contrib import admin
 from django.urls import path 
 from django.urls import include 
 from rango import views
-from django.conf import setting
-from django.conf.urls.static import static
 
 urlpatterns = [
 	path('', views.index, name='index'), 
 	path('rango/', include('rango.urls')), 
 	path('admin/', admin.site.urls), 
-	
-] + static(settings.MEDIA_URL, document_root=setting.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
