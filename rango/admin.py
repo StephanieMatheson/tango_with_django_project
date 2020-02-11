dad87aae2ce8190cf3dc13670476c81b4371e802
Stephanie Matheson <stephanie.matheson79@gmail.com>
2020-02-11 17:06:26 +0000

    Completed chapter 6, passed tests

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-
-
-# Register your models here.
 from rango.models import Category, Page
 
-admin.site.register(Category)
+# Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin): 
+	prepopulated_fields = {'slug':('name',)}
+	
+# Update the registration to include this customised interface 
+admin.site.register(Category, CategoryAdmin) 
+
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
 	
 admin.site.register(Page, PageAdmin)
+
